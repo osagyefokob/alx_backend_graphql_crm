6549bd1d724873dcac010cd5e6cca1306d3eb1b7
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-12-14 04:19:03 +0000

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,10 +1,11 @@
 import graphene
-from crm.schema import Query as CRMQuery
 
 
-class Query(CRMQuery, graphene.ObjectType):
-    pass
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(self, info):
+        return "Hello, GraphQL!"
 
 
 schema = graphene.Schema(query=Query)
-
