6fa8c604c2e8b33daf33f7218e39f4f11f970c17
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-12-14 04:28:45 +0000

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,11 +1,13 @@
 import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
 
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-
-    def resolve_hello(self, info):
-        return "Hello, GraphQL!"
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
